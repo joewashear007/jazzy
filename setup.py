2cdb4d6eca5d81cbab709ca66fc2841382e6fe86
Joseph Livecchi <joewashear007@gmail.com>
2015-02-10 10:33:12 -0500

    Added Python Skeleton Structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'jazzy - jaz language interperter',
+    'author': 'Joseph Livecchi, Antohny Bier, ',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'joewashear007@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['jazzy'],
+    'scripts': [],
+    'name': 'jazzy'
+}
+
+setup(**config)
