57cbffacadd4eb6d9dd9fc4e01c94719a37c5eef
nuraalkhateeb <nuraalkhateeb@gmail.com>
2015-02-26 12:49:10 -0500

    Added commands - no functionality yet.

--- jazzy/functions/CtrlFunc.py
+++ jazzy/functions/CtrlFunc.py
@@ -1,4 +1,4 @@
-__all__ = ['jazHalt']
+__all__ = ['jazHalt', 'jazLabel', 'jazGoto', 'jazGofalse', 'jazGotrue']
 
 
 class jazHalt:
@@ -8,7 +8,34 @@ class jazHalt:
     def call(self, interpreter, arg):
         return interpreter.Halt()
 
+class jazLabel:
+    def __init__(self):
+        self.command = "label";
 
+    def call(self, interpreter, arg):
+        return none
+
+class jazGoto:
+    def __init__(self):
+        self.command = "goto";
+
+    def call(self, interpreter, arg):
+        return none
+
+class jazGofalse:
+    def __init__(self):
+        self.command = "gofalse";
+
+    def call(self, interpreter, arg):
+        return none
+
+class jazGotrue:
+    def __init__(self):
+        self.command = "gotrue";
+
+    def call(self, interpreter, arg):
+        return none
+        
 # A dictionary of the classes in this file
 # used to autoload the functions
-Functions = {'jazHalt': jazHalt}
+Functions = {'jazHalt': jazHalt, 'jazLabel': jazLabel, 'jazGoto': jazGoto, 'jazGofalse': jazGofalse, 'jazGotrue': jazGotrue}
