d527bf1092f085b6e8f33d7dc8e15a89bc6dd50f
Anthony <anthony.bier@rockets.utoledo.edu>
2015-03-01 16:25:12 -0500

    relational functions
    here are the relational functions tell me if you don't like the naming
    convention
    G>
    L>
    E=
    Than,to T

---
+++ jazzy/functions/relationalFunc.py
@@ -0,0 +1,81 @@
+__all__ = ['jazGT', 'jazET', 'jazGTET', 'jazLT', 'jazLTET', 'jazGL']
+
+class jazGT:
+    def __init__(self):
+        self.command = ">";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 > topValue2):
+            interpreter.GetScope().stack.append(1)
+        else:
+            interpreter.GetScope().stack.append(0)
+        return None
+        
+class jazET:
+    def __init__(self):
+        self.command = "=";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 == topValue2):
+            interpreter.GetScope().stack.append(1)
+        else:
+            interpreter.GetScope().stack.append(0)
+        return None
+    
+class jazGTET:
+    def __init__(self):
+        self.command = ">=";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 >= topValue2):
+            interpreter.GetScope().stack.append(1)
+        else:
+            interpreter.GetScope().stack.append(0)
+        return None
+    
+class jazLT:
+    def __init__(self):
+        self.command = "<";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 < topValue2):
+            interpreter.GetScope().stack.append(1)
+        else:
+            interpreter.GetScope().stack.append(0)
+        return None
+ 
+class jazLTET:
+    def __init__(self):
+        self.command = "<=";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 <= topValue2):
+            interpreter.GetScope().stack.append(1)
+        else:
+            interpreter.GetScope().stack.append(0)
+        return None
+         
+class jazGL:
+    def __init__(self):
+        self.command = "<>";
+        
+    def call(self, interpreter, arg):
+        topValue1 = interpreter.GetScope().stack[-2]
+        topValue2 = interpreter.GetScope().stack[-1]
+        if (topValue1 == topValue2):
+            interpreter.GetScope().stack.append(0)
+        else:
+            interpreter.GetScope().stack.append(1)
+        return None
+    
+Functions = {'jazGT': jazGT, 'jazET': jazET, 'jazGTET': jazGTET, 'jazLT':jazLT, 'jazLTET':jazLTET, 'jazGL':jazGL}
